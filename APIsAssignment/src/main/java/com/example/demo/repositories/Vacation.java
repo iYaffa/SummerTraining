8600686a1fcf31293c5ab4384d001dc815294903
iYaffa <yafawx@gmail.com>
2025-08-26 15:46:53 +0300

    vacation class

--- APIsAssignment/src/main/java/com/example/demo/repositories/Vacation.java
+++ APIsAssignment/src/main/java/com/example/demo/repositories/Vacation.java
@@ -7,4 +7,53 @@ public class Vacation {
     private String startDate;
     private String endDate;
     private String status;
+
+    public Vacation(int vacationId, int employeeId, String startDate, String endDate, String status) {
+        this.vacationId = vacationId;
+        this.employeeId = employeeId;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.status = status;
+    }
+
+    public int getVacationId() {
+        return vacationId;
+    }
+
+    public void setVacationId(int vacationId) {
+        this.vacationId = vacationId;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
 }
