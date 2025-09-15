2769cd55285cbc9c47b4621f21f6cfe3e0b081e9
iYaffa <yafawx@gmail.com>
2025-09-15 18:17:49 +0300

    adding telegram dependencies

---
+++ telegram_api/src/main/java/com/example/telegram_api/TelegramApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.telegram_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramApiApplication.class, args);
+	}
+
+}
